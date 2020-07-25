976862ee96ece4b8327eb95559a1ca724d4a8314
Jonas <kvaek@kvaek.eu>
2020-07-26 01:35:18 +0200

    .

---
+++ pages/smuggle.js
@@ -0,0 +1,9 @@
+import Depiction from '../components/Depiction'
+
+export default function smuggle() {
+	return (
+		<Depiction name="Smuggle" github="https://github.com/Kvaek/Smuggle" subtitle="Customizable hiding of apps">
+			This tweak grants you the ability to <span style={{ color: 'green' }}>hide</span> apps from the <span style={{ color: 'blue' }}>app switcher</span>, <span style={{ color: 'green' }}>homescreen</span>,<span style={{ color: 'green' }}>and more</span>.
+		</Depiction>
+	)
+}
