78f1420198bcfc2e7fd6ff141ea150671f55a1da
Jonas <kvaek@kvaek.eu>
2020-07-26 01:07:00 +0200

    resonance

--- repo.js
+++ repo.js
@@ -3,8 +3,10 @@ exports.description = "Kvaek's tweaks n' stuff"
 
 exports.packages = [
 	'https://github.com/Kvaek/Repository/releases/download/1.1/eu.kvaek.smuggle_1.1_iphoneos-arm.deb',
+	'https://github.com/Kvaek/Repository/releases/download/1.0/eu.kvaek.resonance_1.0-1+debug_iphoneos-arm.deb',
 ]
 
 exports.icons = {
 	Smuggle: 'assets/smuggle/icon.png',
+	Resonance: 'assets/resonance/icon.png',
 }
