2dec279fb89dd0a07672606e44ed7f2caf637dc8
Jonas <kvaek@kvaek.eu>
2020-07-26 00:52:15 +0200

    .

--- repo.js
+++ repo.js
@@ -1,15 +1,10 @@
-exports.name = 'Repo on Vercel'
-exports.description = "Repo on Vercel's example repository"
+exports.name = 'Kvaek'
+exports.description = "Kvaek's tweaks n' stuff"
 
 exports.packages = [
-	'https://github.com/dimitarnestorov/RepoOnVercel/releases/download/red-1.0.1/example.red_1.0.1_iphoneos-arm.deb',
-	'https://github.com/dimitarnestorov/RepoOnVercel/releases/download/red-1.0.0/example.red_1.0.0_iphoneos-arm.deb',
-	'https://github.com/dimitarnestorov/RepoOnVercel/releases/download/green-1.0.0/example.green_1.0.0_iphoneos-arm.deb',
-	'https://github.com/dimitarnestorov/RepoOnVercel/releases/download/blue-1.0.0/example.blue_1.0.0_iphoneos-arm.deb',
+	'https://github.com/Kvaek/Repository/releases/download/1.1/eu.kvaek.smuggle_1.1_iphoneos-arm.deb',
 ]
 
 exports.icons = {
-	Red: 'assets/red/icon.png',
-	Blue: 'assets/blue/icon.png',
-	Green: 'assets/green/icon.png',
+	Smuggle: 'assets/smuggle/icon.png',
 }
